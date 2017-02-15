b1fc0a1c862e257d52bb729020ecb308b06b0245
Fullmer <jmf3658@ITS-APP-889109.local>
2017-02-14 19:44:01 -0600

    Complete D8 port

--- js/bootstrap_modal_messages.js
+++ js/bootstrap_modal_messages.js
@@ -6,6 +6,7 @@
   /**
    * Function to open the modal.
    */
+
   var bootstrap_modal_messages_open = function($settings, $controls_click) {
     // Controls were clicked to open modal.
     if ($controls_click) {
@@ -39,89 +40,93 @@
   }
 
   // Main loop.
-  Drupal.behaviors.bootstrap_modal_messages = {
-    attach: function (context, settings) {
-      var $settings = settings.bootstrap_modal_messages || {};
-      var $body = $(document.body);
-      var $messages_html = 'No messages';
-      $body.once('bootstrap-modal-messages', function() {
-        // Let's not deal with issues where the person doesn't have BS JS.
-        if (!$.isFunction($.fn.modal)) {
-          return;
-        }
-        // Set 1/0 to true/false respectively.
-        $.each($settings, function(key, value) {
-          if (value == 0) {
-            $settings[key] = false;
-          }
-          if (value == 1) {
-            $settings[key] = true;
-          }
-        });
+  var $settings = drupalSettings.bootstrap_modal_messages || {};
+  var $body = $(document.body);
+  var $messages_html = 'No messages';
 
-        var $modal_content = $('<div class="modal fade" id="bmm-modal" tabindex="-1" role="dialog" aria-labelledby="bmm-label" aria-hidden="true">'
-          + '<div class="modal-dialog">'
-          + '<div class="modal-content">');
+  if (!$.isFunction($.fn.modal)) {
+    return;
+  }
 
-        // Modal header.
-        if ($settings.show_header) {
-          $modal_content.find('.modal-content').append('<div class="modal-header" id="bmm-label">');
-          if ($settings.header_close) {
-            $modal_content.find('.modal-header').append('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>');
-          }
-          $modal_content.find('.modal-header').append($settings.title);
-        }
+  $.each($settings, function(key, value) {
+    if (value == 0) {
+      $settings[key] = false;
+    }
+    if (value == 1) {
+      $settings[key] = true;
+    }
+  });
 
-        // Modal body.
-        switch ($settings.multiple) {
-          // All messages in 1 modal.
-          case 'single':
-            if ($body.find($settings.selector).length > 0) {
-              $messages_html = '';
-              $body.find($settings.selector).each(function(index) {
-                $messages_html += $(this).detach().wrap('<p/>').parent().html();
-              });
-            }
-            $modal_content.find('.modal-content').append('<div class="modal-body">' + $messages_html + '</div>');
-          break;
-          // Multiple modals per message type.
-          case 'multiple':
-            $settings.messages = [];
-            $settings.messages_storage = [];
-            $body.find($settings.selector).each(function(index) {
-              $settings.messages_storage[index] = $settings.messages[index] = $(this).detach().wrap('<p/>').parent().html();
-            });
-            if ($settings.messages.length > 0) {
-              $messages_html = $settings.messages[0];
-              $settings.messages.shift();
-            }
-            $modal_content.find('.modal-content').append('<div class="modal-body">' + $messages_html + '</div>');
-          break;
-        }
+  console.log($settings);
 
-        // Modal footer.
-        if ($settings.show_footer) {
-          $modal_content.find('.modal-content').append('<div class="modal-footer">' + $settings.footer_html + '</div>');
-        }
+  var $modal_content = $('<div class="modal fade" id="bmm-modal" tabindex="-1" role="dialog" aria-labelledby="bmm-label" aria-hidden="true">'
+    + '<div class="modal-dialog">'
+    + '<div class="modal-content">');
 
-        // Now add it to the page.
-        $body.append($modal_content);
+    // Modal header.
+    if ($settings.show_header) {
+      $modal_content.find('.modal-content').append('<div class="modal-header" id="bmm-label">');
+      if ($settings.header_close) {
+        $modal_content.find('.modal-header').append('<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>');
+      }
+      $modal_content.find('.modal-header').append($settings.title);
+    }
 
-        // Controls.
-        if ($settings.show_controls) {
-          var $controls = $('<div class="bmm-modal-controls">' + $settings.controls_html + '</div>');
-          $body.append($controls);
-          $controls.on('click', function() {
-            bootstrap_modal_messages_open($settings, true);
+    // Modal body.
+    switch ($settings.multiple) {
+      // All messages in 1 modal.
+      case 'single':
+        if ($body.find($settings.selector).length > 0) {
+          $messages_html = '';
+          $body.find($settings.selector).each(function(index) {
+            $messages_html += $(this).detach().wrap('<p/>').parent().html();
           });
         }
-
-        // Show on load.
-        if ($settings.show_onload && $messages_html != 'No messages') {
-          bootstrap_modal_messages_open($settings, false);
+        $modal_content.find('.modal-content').append('<div class="modal-body">' + $messages_html + '</div>');
+      break;
+      // Multiple modals per message type.
+      case 'multiple':
+        $settings.messages = [];
+        $settings.messages_storage = [];
+        $body.find($settings.selector).each(function(index) {
+          $settings.messages_storage[index] = $settings.messages[index] = $(this).detach().wrap('<p/>').parent().html();
+        });
+        if ($settings.messages.length > 0) {
+          $messages_html = $settings.messages[0];
+          $settings.messages.shift();
         }
+        $modal_content.find('.modal-content').append('<div class="modal-body">' + $messages_html + '</div>');
+      break;
+    }
 
+    // Modal footer.
+    if ($settings.show_footer) {
+      $modal_content.find('.modal-content').append('<div class="modal-footer">' + $settings.footer_html + '</div>');
+    }
+
+    // Now add it to the page.
+    $body.append($modal_content);
+
+    // Controls.
+    if ($settings.show_controls) {
+      var $controls = $('<div class="bmm-modal-controls">' + $settings.controls_html + '</div>');
+      $body.append($controls);
+      $controls.on('click', function() {
+        bootstrap_modal_messages_open($settings, true);
       });
     }
-  };
+
+    // Show on load.
+    if ($settings.show_onload && $messages_html != 'No messages') {
+        if ($.cookie('bootstrap_modal_messages_onload') == null) {
+        var $time = 0;
+        if ($settings.onload_expiration != null) {
+          $time = $settings.onload_expiration;
+        }
+        var date = new Date();
+        date.setTime(date.getTime() + ($time * 1000));
+        $.cookie("bootstrap_modal_messages_onload", "7", { expires: date });
+        bootstrap_modal_messages_open($settings, false);
+      }
+    }
 }(jQuery));
--- src/Form/BootstrapModalMessagesAdminForm.php
+++ src/Form/BootstrapModalMessagesAdminForm.php
@@ -1,16 +1,15 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\bootstrap_modal_messages\Form\BootstrapModalMessagesAdminForm.
- */
-
 namespace Drupal\bootstrap_modal_messages\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element;
 
+/**
+ * Class BootstrapModalMessagesAdminForm.
+ *
+ * @package Drupal\bootstrap_modal_messages\Form
+ */
 class BootstrapModalMessagesAdminForm extends ConfigFormBase {
 
   /**
@@ -23,159 +22,202 @@ class BootstrapModalMessagesAdminForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $config = $this->config('bootstrap_modal_messages.settings');
-
-    foreach (Element::children($form) as $variable) {
-      $config->set($variable, $form_state->getValue($form[$variable]['#parents']));
-    }
-    $config->save();
-
-    if (method_exists($this, '_submitForm')) {
-      $this->_submitForm($form, $form_state);
-    }
-
-    parent::submitForm($form, $form_state);
+  protected function getEditableConfigNames() {
+    return ['bootstrap_modal_messages.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
-    return ['bootstrap_modal_messages.settings'];
-  }
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('bootstrap_modal_messages.settings');
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    // @todo Option to use with any sourced BS library.
-    $form['bootstrap_modal_messages_selector'] = [
+    $form['bootstrap_modal_messages_selector'] = array(
       '#type' => 'textfield',
       '#title' => t('Messages div selector'),
-      '#default_value' => variable_get('bootstrap_modal_messages_selector', '.messages'),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_selector'),
       '#description' => t('The CSS/JS selector to find messages for the modal.'),
-    ];
+    );
 
-    $form['bootstrap_modal_messages_multiple'] = [
+    $form['bootstrap_modal_messages_multiple'] = array(
       '#type' => 'select',
       '#title' => t('Multiple messages setting'),
-      '#options' => [
+      '#options' => array(
         'single' => t('Single modal'),
         'multiple' => t('Multiple - New modal per message type'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_multiple', 'single'),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_multiple'),
       '#description' => t('How to handle multiple messages on one page?'),
-    ];
+    );
 
-    $form['bootstrap_modal_messages_ignore_admin'] = [
+    $form['bootstrap_modal_messages_ignore_admin'] = array(
       '#type' => 'select',
       '#title' => t('Ignore admin pages'),
-      '#options' => [
+      '#options' => array(
         1 => t('Yes'),
         0 => t('No'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_ignore_admin', 1),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_ignore_admin'),
       '#description' => t('Ignore admin pages so that messages display normally?'),
-    ];
+    );
 
     // Modal header.
-    $form['modal_header'] = [
+    $form['modal_header'] = array(
       '#type' => 'fieldset',
       '#title' => t('Modal Header'),
       '#collapsible' => TRUE,
-    ];
+    );
 
-    $form['modal_header']['bootstrap_modal_messages_show_header'] = [
+    $form['modal_header']['bootstrap_modal_messages_show_header'] = array(
       '#type' => 'select',
       '#title' => t('Show header'),
-      '#options' => [
+      '#options' => array(
         1 => t('Yes'),
         0 => t('No'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_show_header', 1),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_show_header'),
       '#description' => t('Show the modal header.'),
-    ];
+    );
 
-    $form['modal_header']['bootstrap_modal_messages_title'] = [
-      '#type' => 'textarea',
+    $title = $config->get('bootstrap_modal_messages_title');
+    $form['modal_header']['bootstrap_modal_messages_title'] = array(
+      '#type' => 'text_format',
       '#title' => t('Modal Title'),
-      '#default_value' => variable_get('bootstrap_modal_messages_title', BOOTSTRAP_MODAL_MESSAGES_HEADER),
-      '#description' => t('Enter the HTML to be used in the modal title. Defaults to Bootstrap\'s default h4 title.'),
-    ];
+      '#default_value' => $title['value'] ?: '',
+      '#format' => $title['format'] ?: 'plain_text',
+      '#description' => t('Enter text to be used in the modal title. Defaults to "Messages".'),
+      '#states' => array(
+        'visible' => array(
+          ':input[name="bootstrap_modal_messages_show_header"]' => array('value' => 1),
+        ),
+      ),
+    );
 
-    $form['modal_header']['bootstrap_modal_messages_header_close'] = [
+    $form['modal_header']['bootstrap_modal_messages_header_close'] = array(
       '#type' => 'select',
       '#title' => t('Show close button (X)'),
-      '#options' => [
+      '#options' => array(
         1 => t('Yes'),
         0 => t('No'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_header_close', 1),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_header_close'),
       '#description' => t('Show the close (X) button in modal header.'),
-    ];
+    );
 
     // Modal footer.
-    $form['modal_footer'] = [
+    $form['modal_footer'] = array(
       '#type' => 'fieldset',
       '#title' => t('Modal Footer'),
       '#collapsible' => TRUE,
-    ];
+    );
 
-    $form['modal_footer']['bootstrap_modal_messages_show_footer'] = [
+    $form['modal_footer']['bootstrap_modal_messages_show_footer'] = array(
       '#type' => 'select',
       '#title' => t('Show footer'),
-      '#options' => [
+      '#options' => array(
         1 => t('Yes'),
         0 => t('No'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_show_footer', 1),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_show_footer'),
       '#description' => t('Show the modal footer.'),
-    ];
+    );
 
-    $form['modal_footer']['bootstrap_modal_messages_footer_html'] = [
-      '#type' => 'textarea',
+    $footer = $config->get('bootstrap_modal_messages_footer_html');
+    $form['modal_footer']['bootstrap_modal_messages_footer_html'] = array(
+      '#type' => 'text_format',
       '#title' => t('Footer HTML'),
-      '#default_value' => variable_get('bootstrap_modal_messages_footer_html', BOOTSTRAP_MODAL_MESSAGES_FOOTER),
-      '#description' => t('Enter the HTML to be used in the modal footer. Defaults to Bootstrap\'s default Close button.'),
-    ];
+      '#default_value' => $footer['value'] ?: '',
+      '#format' => $footer['format'] ?: 'plain_text',
+      '#description' => t("Enter text to be displayed in the footer. Leave empty to use Bootstrap's default Close button."),
+      '#states' => array(
+        'visible' => array(
+          ':input[name="bootstrap_modal_messages_show_footer"]' => array('value' => 1),
+        ),
+      ),
+    );
 
     // Modal controls.
-    $form['controls'] = [
+    $form['controls'] = array(
       '#type' => 'fieldset',
       '#title' => t('Controls'),
       '#collapsible' => TRUE,
-    ];
+    );
 
-    $form['controls']['bootstrap_modal_messages_show_onload'] = [
+    $form['controls']['bootstrap_modal_messages_show_onload'] = array(
       '#type' => 'select',
       '#title' => t('Open modal on page load?'),
-      '#options' => [
+      '#options' => array(
         1 => t('Yes'),
         0 => t('No - Should enable "Show controls" to display modal'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_show_onload', 1),
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_show_onload'),
       '#description' => t('Open modal immediately when the page loads.'),
-    ];
+    );
 
-    $form['controls']['bootstrap_modal_messages_show_controls'] = [
+    $form['controls']['bootstrap_modal_messages_onload_expiration'] = array(
+      '#type' => 'number',
+      '#title' => t('Time (in seconds) browser waits to display messages again.'),
+      '#min' => 0,
+      '#max' => 86400,
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_onload_expiration'),
+      '#description' => t('Upon subsequent page loads, the "show onload" will not happen until this time has elapsed. Useful for single, sitewide messages.'),
+      '#states' => array(
+        'visible' => array(
+          ':input[name="bootstrap_modal_messages_show_onload"]' => array('value' => 1),
+        ),
+      ),
+    );
+
+    $form['controls']['bootstrap_modal_messages_show_controls'] = array(
       '#type' => 'select',
       '#title' => t('Show controls?'),
-      '#options' => [
+      '#options' => array(
         0 => t('No'),
         1 => t('Yes'),
-      ],
-      '#default_value' => variable_get('bootstrap_modal_messages_show_controls', 0),
-      '#description' => t('Creates a div that allows you to show messages again. !note', [
-        '!note' => '<strong>This setting can be overridden by the permission "View Bootstrap Modal Messages controls".</strong>'
-        ]),
-    ];
+      ),
+      '#default_value' => \Drupal::config('bootstrap_modal_messages.settings')->get('bootstrap_modal_messages_show_controls'),
+      '#description' => t('Creates a div that allows you to show messages again. <strong>This setting can be overridden by the permission "View Bootstrap Modal Messages controls".</strong>'),
+    );
 
-    $form['controls']['bootstrap_modal_messages_controls_html'] = [
-      '#type' => 'textarea',
+    $controls = $config->get('bootstrap_modal_messages_controls_html');
+    $form['controls']['bootstrap_modal_messages_controls_html'] = array(
+      '#type' => 'text_format',
       '#title' => t('Controls HTML'),
-      '#default_value' => variable_get('bootstrap_modal_messages_controls_html', t('Messages')),
+      '#default_value' => $controls['value'] ?: '',
+      '#format' => $controls['format'] ?: 'plain_text',
       '#description' => t('Enter the HTML to be used in the div for controls.'),
-    ];
+      '#states' => array(
+        'visible' => array(
+          ':input[name="bootstrap_modal_messages_show_controls"]' => array('value' => 1),
+        ),
+      ),
+    );
 
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $values = $form_state->getValues();
+    $this->config('bootstrap_modal_messages.settings')
+      ->set('bootstrap_modal_messages_selector', $values['bootstrap_modal_messages_selector'])
+      ->set('bootstrap_modal_messages_multiple', $values['bootstrap_modal_messages_multiple'])
+      ->set('bootstrap_modal_messages_ignore_admin', $values['bootstrap_modal_messages_ignore_admin'])
+      ->set('bootstrap_modal_messages_show_header', $values['bootstrap_modal_messages_show_header'])
+      ->set('bootstrap_modal_messages_title', $values['bootstrap_modal_messages_title'])
+      ->set('bootstrap_modal_messages_header_close', $values['bootstrap_modal_messages_header_close'])
+      ->set('bootstrap_modal_messages_show_footer', $values['bootstrap_modal_messages_show_footer'])
+      ->set('bootstrap_modal_messages_footer_html', $values['bootstrap_modal_messages_footer_html'])
+      ->set('bootstrap_modal_messages_show_onload', $values['bootstrap_modal_messages_show_onload'])
+      ->set('bootstrap_modal_messages_show_controls', $values['bootstrap_modal_messages_show_controls'])
+      ->set('bootstrap_modal_messages_controls_html', $values['bootstrap_modal_messages_controls_html'])
+      ->set('bootstrap_modal_messages_onload_expiration', $values['bootstrap_modal_messages_onload_expiration'])
+      ->save();
+
+    parent::submitForm($form, $form_state);
   }
 
 }
